43ee1ec108e52752df8733601ed19567dff6c304
kannan <kannans89@hotmail.com>
2026-09-24 00:13:36 +0530

    'test'

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Hello TCS Folks welcome to ACR v3.0</h1>"
+    return "<h1>Hello TCS Folks welcome to ACR v4.0,integrated with github</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
